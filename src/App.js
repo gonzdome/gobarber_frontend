b02a491753496bec6fd2b96a8ac3b70588568e5e
Gabriel Gonzalez Domeniconi <84802045+gonzdome@users.noreply.github.com>
2022-01-07 11:04:18 -0300

    Removed unused text.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          ATUMALACA KKKKK
-        </p>
+        <p></p>
       </header>
     </div>
   );
